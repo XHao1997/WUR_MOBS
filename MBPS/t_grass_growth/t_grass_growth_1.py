21cf95f9d1fef0ab8c66d54f0dfb41afc664577e
Xu, Hao <hao.xu@wur.nl>
2023-11-08 16:12:34 +0100

    upload grass model tutorial

---
+++ MBPS/t_grass_growth/t_grass_growth_1.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+FTE34806 - Modelling of Biobased Production Systems
+MSc Biosystems Engineering, WUR
+@authors:   --- Fill in your team names ---
+
+Tutorial: Grass growth model analysis.
+1. Light intensity over leaves.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+### 1. Light intensity over leaves
+# TODO: Define a function f_Il
+# which takes positional arguments l, k, m, I0
+# and returns an array for Il
+def f_Il ???
+    ???
+    return Il
+
+# TODO: define an array with sensible values of leaf area index (l)
+# Use the function np.linspace
+l = ???
+
+# TODO: Code the figures for:
+    # 1) Il vs. l, with three test values for k 
+    # 2) Il vs. l, with three test values for m
+    # 3) Il vs. l, with three test values for I0
+plt.style.use('ggplot')
+plt.figure(1)
+
+
+plt.figure(2)
+
+
+plt.figure(3)
+
---
+++ MBPS/t_grass_growth/t_grass_growth_3.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+FTE34806 - Modelling of Biobased Production Systems
+MSc Biosystems Engineering, WUR
+@authors:   -- Fill in your team names --
+
+Tutorial: Grass growth model analysis.
+3. Temperature index
+"""
+# TODO: Import the required packages 
+
+
+# TODO: Define the values for the TI parameters
+
+
+# TODO: Define a sensible array for values of T
+
+
+# TODO: (Optional) Define support variables DTmin, DTmax, DTa, DTb
+# Temperature index: TI = ( (DTmax/DTa) * (DTmin/DTb)**(DTb/DTa) )**z
+
+
+# TODO: Define TI
+
+
+# TODO: Make a plot for TI vs T
