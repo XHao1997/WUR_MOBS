a709d04c5f63f618d0c623a988d244dc78854be8
Hao Xu <xuhao6815@gmail.com>
2023-11-13 19:23:11 +0100

    update the grass mass array

--- MBPS/MBPS_startcourse/mbps/case1_files/grass_eval.py
+++ MBPS/MBPS_startcourse/mbps/case1_files/grass_eval.py
@@ -25,6 +25,8 @@ data_weather = pd.read_csv(
     usecols = ['YYYYMMDD', 'TG', 'Q', 'RH'], # columns to use
     index_col = 0, # column with row names from used columns, 0-indexed
     )
+data_grass = pd.read_csv("../../data/temp_real.csv",usecols=['X','Y'])
+t_grass_data = np.arange(0,252,0.1)
 
 # data_weather = pd.read_csv(
 #     '../../data/temp.csv', #  to move up one directory from current directory
@@ -40,8 +42,11 @@ plt.style.use('ggplot')
 
 # Grass data
 # TODO: define numpy arrays with measured grass data in the Netherlands
-t_grass_data = np.array([])
-m_grass_data = np.array([])
+t_grass_data = np.arange(0,252,0.1)
+y_int = np.interp(t_grass_data,data_grass['X'],data_grass['Y'])
+m_grass = np.zeros_like(t_grass_data)
+for i in range(t_grass_data.shape[0]):
+    m_grass[i] = np.trapz(y_int[:i], np.array(t_grass_data[:i]))
 
 # Simulation time
 tsim = np.linspace(0.0, 365.0, 365+1) # [d]
@@ -49,42 +54,42 @@ dt = 1 # [d]
 
 # Initial conditions
 # TODO: define sensible values for the initial conditions
-x0 = {???, ???}  # [kgC m-2]
+# x0 = {???, ???}  # [kgC m-2]
 
 # Model parameters, as provided by Mohtar et al. (1997)
 # TODO: define the varameper values in the dictionary p
-p = { ??? }
+# p = { ??? }
 
 # Model parameters adjusted manually to obtain growth
-# TODO: (Further) adjust 2-3 parameter values to match measured growth behaviour
-p[???] = ???
-p[???] = ???
-
-
-# # Retrieve relevant arrays from pandas dataframe
-T = data_weather.loc[t_ini:t_end,'TG'].values    # [0.1 °C] Env. temperature
-I_gl = data_weather.loc[t_ini:t_end,'Q'].values # [J cm-2 d-1] Global irradiance
+# # TODO: (Further) adjust 2-3 parameter values to match measured growth behaviour
+# p[???] = ???
+# p[???] = ???
+#
+#
+# # # Retrieve relevant arrays from pandas dataframe
+# T = data_weather.loc[t_ini:t_end,'TG'].values    # [0.1 °C] Env. temperature
+# I_gl = data_weather.loc[t_ini:t_end,'Q'].values # [J cm-2 d-1] Global irradiance
 # # Aply the necessary conversions of units
-
-T = T/10    # [???] to [???] Env. temperature
-I0 = I_gl/(100**2)  # [J/cm2] to [W/m2] Global irradiance to PAR
-# Dictionary of disturbances (2D arrays, with col 1 for time, and col 2 for d)
-d = {'T':np.array([tsim, T]).T,
-     'I0':np.array([tsim, I0]).T,
-     'WAI':np.array([tsim, np.full((tsim.size,),1.0)]).T
-     }
-
-# Controlled inputs
-u = {'f_Gr':0, 'f_Hr':0}            # [kgDM m-2 d-1]
-
-# Initialize module
-# TODO: Call the module Grass to initialize an instance
-grass = Grass(tsim, dt, x0, p)
-
-# Run simulation
-# TODO: Call the method run to generate simulation results
-tspan = (tsim[0], tsim[-1])
-y_grass = grass.run(???, ???, ???)
+#
+# T = T/10    # [???] to [???] Env. temperature
+# I0 = I_gl/(100**2)  # [J/cm2] to [W/m2] Global irradiance to PAR
+# # Dictionary of disturbances (2D arrays, with col 1 for time, and col 2 for d)
+# d = {'T':np.array([tsim, T]).T,
+#      'I0':np.array([tsim, I0]).T,
+#      'WAI':np.array([tsim, np.full((tsim.size,),1.0)]).T
+#      }
+#
+# # Controlled inputs
+# u = {'f_Gr':0, 'f_Hr':0}            # [kgDM m-2 d-1]
+#
+# # Initialize module
+# # TODO: Call the module Grass to initialize an instance
+# grass = Grass(tsim, dt, x0, p)
+#
+# # Run simulation
+# # TODO: Call the method run to generate simulation results
+# tspan = (tsim[0], tsim[-1])
+# y_grass = grass.run(???, ???, ???)
 
 # # Retrieve simulation results
 # # assuming 0.4 kgC/kgDM (Mohtar et al. 1997, p. 1492)
@@ -97,3 +102,11 @@ y_grass = grass.run(???, ???, ???)
 # # TODO: Make a plot for WsDM, WgDM and grass measurement data.
 # plt.figure(1)
 #
+
+fig, ax = plt.subplots()
+ax.set_xlabel(r'day', fontsize=10)
+ax.set_ylabel(u'grass mass ($\mathregular{kgCm^{-2}}$)', fontsize=10)
+ax.set_title('grass yield per day')
+ax.scatter(t_grass_data,m_grass,s=1)
+plt.tight_layout()
+plt.show()
